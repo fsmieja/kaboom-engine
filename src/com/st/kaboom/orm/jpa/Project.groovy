422658e7e9ea5244dbbfd48de5a8f4520043b95d
Frank Smieja <frank.smieja@smartatech.com>
2015-03-08 15:31:09 +0000

    added note and project jpas

--- src/com/st/kaboom/orm/jpa/Project.groovy
+++ src/com/st/kaboom/orm/jpa/Project.groovy
@@ -3,7 +3,6 @@ package com.st.kaboom.orm.jpa
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
-import javax.persistence.JoinColumn
 import javax.persistence.Table
 import javax.persistence.Temporal
 import javax.persistence.TemporalType
@@ -13,25 +12,21 @@ import org.springframework.data.annotation.Id
 
 
 @Entity
-@Table( name = "notes" )
-class Note {
+@Table( name = "projects" )
+class Project {
 
 	@Id
 	@GeneratedValue(generator="increment")
 	@GenericGenerator(name="increment", strategy = "increment")
 	Long id
 
-	String title
-	String content
-	String location
+	String name
+	String summary
+	String description
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "created_at")
 	Date created_at
 
-	// @ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "project_id", nullable = false)
-	Project project
-	//Integer project_id
 }
 
